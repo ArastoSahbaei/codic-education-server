a9d0712786b9d5c1244c1b0645fb6b03766f539f
arasto <Arasto.Sahbaei@gmail.com>
2021-03-05 11:07:14 +0100

    removed bodyparser

--- Server.js
+++ Server.js
@@ -2,7 +2,6 @@ import express from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
 import cors from 'cors'
-import bodyParser from 'body-parser'
 import passport from 'passport'
 
 import Configuration from './configurations/Configurations.js'
@@ -18,8 +17,7 @@ import passportConfig from './configurations/passport-config.js'
 const application = express()
 application.use(passport.initialize())
 application.use(cors({ credentials: true }))
-application.use(bodyParser.urlencoded({ extended: true }))
-application.use(bodyParser.json())
+application.use(express.json())
 application.use(helmet())
 application.use(morgan('common'))
 
