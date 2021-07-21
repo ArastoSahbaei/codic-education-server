2722c89ef1eca867522d1e02c4103fbc13135598
arasto <Arasto.Sahbaei@gmail.com>
2021-07-21 13:23:37 +0200

    update

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -34,9 +34,9 @@ const userSchema = Schema({
 		country: { type: String, default: '' },
 		adress: { type: String, default: '' },
 		secondaryAdress: { type: String, default: '' },
-		ZIPcode: { type: String, default: '' },
+		zipCode: { type: String, default: '' },
 		county: { type: String, default: '' },
-		postOrt: { type: String, default: '' },
+		postOffice: { type: String, default: '' },
 		phone: { type: String, default: '' },
 		secondaryPhone: { type: String, default: '' }
 	},
