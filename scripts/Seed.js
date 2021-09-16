5b36853518e067000ef99db129b6374113c7211e
ItsSteph <steph.tronier.vip@gmail.com>
2021-09-16 14:23:25 +0200

    removed unessesary code

--- scripts/Seed.js
+++ scripts/Seed.js
@@ -1,23 +1,6 @@
 import employees from '../src/models/Employee.model.js'
 import { employeeList } from './data/employees.js'
-// import dotenv from 'dotenv'
 
-// dotenv.config()
-// const {
-// 	DEV_DATABASE_URL,
-// 	PROD_DATABASE_URL,
-// 	ENVIROMENT
-// } = process.env
-
-// const connectToDatabase = async () => {
-// 	const DATABASE_URL = ENVIROMENT === 'DEVELOPMENT' ? DEV_DATABASE_URL : PROD_DATABASE_URL
-// 	try {
-// 		await mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
-// 		console.log('✔️  SUCESSFULLY CONNECTED TO DATABASE..')
-// 	} catch (error) {
-// 		console.log('❌  ERROR OCCURED WHILE TRYING TO CONNECT TO THE DATABASE..')
-// 		process.exit()
-// 	}
 
 const seedToDatabase = async () => {
 
