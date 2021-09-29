74c77b0d31ee71f3e9e9cdd5ebeb034aa809bdbc
arasto <Arasto.Sahbaei@gmail.com>
2021-09-29 08:56:57 +0200

    fixed

--- src/routes/Applicant.routes.js
+++ src/routes/Applicant.routes.js
@@ -1,9 +1,9 @@
-import ApplicantController from "../controllers/Applicant.controller.js";
-
+import ApplicantController from "../controllers/Applicant.controller.js"
 
 const routes = application => {
-    application.post('/applicant', ApplicantController.createApplyforCareer )
-    application.get('/applicant', ApplicantController.getAllApplicants)
-    application.get('/applicant/:applicantId', ApplicantController.getApplicantWithId)
+	application.post('/applicant', ApplicantController.createApplyforCareer)
+	application.get('/applicant', ApplicantController.getAllApplicants)
+	application.get('/applicant/:applicantId', ApplicantController.getApplicantWithId)
 }
+
 export default { routes }
