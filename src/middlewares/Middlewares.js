c18bf06ea3a359ce8eeb700dd9464be877027ad4
arasto <Arasto.Sahbaei@gmail.com>
2021-10-11 16:29:48 +0200

    removed unused function

--- src/middlewares/Middlewares.js
+++ src/middlewares/Middlewares.js
@@ -33,26 +33,8 @@ const checkToken = (request, response, next) => {
 	}
 }
 
-const authenticateBasicUser = (request, response, next) => {
-	const header = request.headers['authorization']
-	if (typeof header !== 'undefined') {
-		const bearer = header.split(' ')
-		const token = bearer[1]
-		const verified = jwt.verify(token, 'jwtSecret.secret')
-		const isCorrectUser = request.body.id && request.body.id === verified.id
-		if (isCorrectUser) {
-			next()
-		} else {
-			response.sendStatus(StatusCode.FORBIDDEN)
-		}
-	} else {
-		response.sendStatus(StatusCode.FORBIDDEN)
-	}
-}
-
 export default {
 	notFound,
 	errorHandler,
 	checkToken,
-	authenticateBasicUser
 }
