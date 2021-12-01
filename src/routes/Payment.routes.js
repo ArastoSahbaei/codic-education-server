8dd58dacf3051ca4b932bf0d27e5676446533771
Sabrina <sabrinafransson@gmail.com>
2021-12-01 14:08:49 +0100

    removed old implementation of paypal

--- src/routes/Payment.routes.js
+++ src/routes/Payment.routes.js
@@ -1,10 +1,7 @@
 import PaymentController from '../controllers/Payment.controller.js'
 
 const routes = application => {
-	application.get('/payment/create-order/:data', PaymentController.createOrder)
-	application.get('/payment/capture-order', PaymentController.captureOrder)
-	application.get('/payment/authorize-order', PaymentController.authorizeOrder)
-	application.get('/payment/cancel', PaymentController.cancelOrder)
+	
 }
 
 export default { routes }
