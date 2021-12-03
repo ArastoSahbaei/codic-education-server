6d275b4c036cd65370e2deed64b6068f99b9517f
Sabrina <sabrinafransson@gmail.com>
2021-12-03 09:34:49 +0100

    put all routes in wrapper

--- Server.js
+++ Server.js
@@ -5,18 +5,7 @@ import cors from 'cors'
 import passport from 'passport'
 import passportConfig from './configurations/passport-config.js'
 import Configuration from './configurations/Configurations.js'
-import UserRoutes from './src/routes/User.routes.js'
-import PaymentRoutes from './src/routes/e-commerce/Payment.routes.js'
-import OrderRoutes from './src/routes/e-commerce/Order.routes.js'
-import ProductRoutes from './src/routes/e-commerce/Product.routes.js'
-import ProductCategoryRoutes from './src/routes/e-commerce/ProductCategory.routes.js'
-import ProductBrandRoutes from './src/routes/e-commerce/ProductBrand.routes.js'
-import ShoppingCartRoutes from './src/routes/e-commerce/ShoppingCart.routes.js'
-import ContactRoutes from './src/routes/Contact.routes.js'
-import NewsLetterSubscriptionRoutes from './src/routes/NewsLetterSubscription.routes.js'
-import AdminRoutes from './src/routes/Admin.routes.js'
-import CareerRoutes from './src/routes/Career.routes.js'
-import ApplicantRoutes from './src/routes/Applicant.routes.js'
+import {RouteController} from './src/Routes.js'
 import { notFound } from './src/middlewares/notFound.js'
 import { errorHandler } from './src/middlewares/errorHandler.js'
 
@@ -27,21 +16,10 @@ application.use(express.json())
 application.use(helmet())
 application.use(morgan('common'))
 
+RouteController(application)
+
 passportConfig.registerUserini()
 passportConfig.login()
-
-UserRoutes.routes(application)
-PaymentRoutes.routes(application)
-OrderRoutes.routes(application)
-ProductRoutes.routes(application)
-ContactRoutes.routes(application)
-ProductCategoryRoutes.routes(application)
-ProductBrandRoutes.routes(application)
-ShoppingCartRoutes.routes(application)
-NewsLetterSubscriptionRoutes.routes(application)
-CareerRoutes.routes(application)
-AdminRoutes.routes(application)
-ApplicantRoutes.routes(application)
 
 application.use(notFound)
 application.use(errorHandler)
